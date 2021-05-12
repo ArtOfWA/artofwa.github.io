266e948dfebbea416047935d53806adbcb45d9e2
Nick Beaugeard <nick@tribedev.com.au>
2021-05-12 16:04:30 +1000

    More Messages

---
+++ web/20210220082216js_/https_/www.scribd.com/options/exposed_to_client99c6.js
@@ -0,0 +1,41 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+
+      if (!window.Scribd) Scribd = {};
+      Scribd.NwayTestsWithWeights = {"explain_scribd_iteration_2019_q3":{"id":1530,"choices":{"3770":{"name":"control","weight":1},"3771":{"name":"advertisement","weight":0},"3772":{"name":"did_you_know_variant_1","weight":0},"3773":{"name":"did_you_know_variant_2","weight":0}}},"recommendation_links_new_tab_2020_q4":{"id":1872,"choices":{"4606":{"name":"control","weight":1},"4607":{"name":"test","weight":1}}},"lohp_updates_2020_q3_v2":{"id":1906,"choices":{"4692":{"name":"new_copy","weight":99},"4693":{"name":"control","weight":1}}},"mobile_doc_page_redesign_2020_q3_v2":{"id":1911,"choices":{"4705":{"name":"control","weight":1},"4706":{"name":"promo","weight":1},"4707":{"name":"metadata","weight":1},"4708":{"name":"recommender","weight":1}}},"personalized_archive_offer":{"id":1615,"choices":{"3993":{"name":"control","weight":0},"3994":{"name":"personalized","weight":93},"3995":{"name":"subscribe_only","weight":1},"3996":{"name":"subscribe_or_upload_1","weight":1},"3997":{"name":"subscribe_or_upload_3","weight":1},"3998":{"name":"subscribe_or_upload_5","weight":1},"3999":{"name":"upload_1_or_subscribe","weight":1},"4000":{"name":"upload_3_or_subscribe","weight":1},"4001":{"name":"upload_5_or_subscribe","weight":1}}},"recs_item_similarity_q4_c":{"id":1909,"choices":{"4699":{"name":"control","weight":1},"4700":{"name":"test","weight":0},"4701":{"name":"test_popularity","weight":0}}},"walled_garden_remove_download":{"id":1899,"choices":{"4678":{"name":"control","weight":1},"4679":{"name":"test","weight":0}}},"doc_page_exit_modal_2020_q2":{"id":1805,"choices":{"4466":{"name":"control","weight":99},"4467":{"name":"search","weight":1},"4468":{"name":"categories","weight":1},"4469":{"name":"cta","weight":1}}}};
+      Scribd.Sentry = {"disabled":false,"dsn":"https://web.archive.org/web/20210220082216/https://21588782029b459685064c03453673ff@sentry.io/282187","environment":"production_r4","page":"","release":"9df94dae9b094ef2f946480d5e1934c3f01ec0bf","queue":{"errors":[],"messages":[]}};
+      Scribd.ServerOptions = {"ad_refresh_engagement_tracking_delay":1200,"ad_refresh_idle_time_before_inactive":60,"ad_refresh_interval":60,"disable_trending":false,"facebook_autologin_enabled":false,"largest_contentful_paint":100,"library_dropdown_test_choices":["control","test"],"library_dropdown_test_enabled":false,"payments_store_disabled":false,"rapid_testing_salt":"eowyn","rapid_testing_traffic_configuration":"{\"bucket_0\":\"control\", \"bucket_1\":\"control\", \"bucket_2\":\"control\", \"bucket_3\":\"control\", \"bucket_4\":\"control\", \"bucket_5\":\"control\", \"bucket_6\":\"control\", \"bucket_7\":\"control\"}","reading_history_scroll_timeout":500,"reading_progress_enabled":true,"reading_progress_throttle_get_milliseconds":60000,"reading_progress_throttle_post_milliseconds":10000,"seo_roadblock_choices":["question_promo","promo","banner"],"seo_roadblock_enabled":true,"seo_roadblock_rollout":1.0,"shadow_autocomplete_enabled":false,"sidebar_promo_enabled":false,"web_free_to_paid_enabled":true};
+    
+
+}
+/*
+     FILE ARCHIVED ON 08:22:16 Feb 20, 2021 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 05:59:07 May 12, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 3546.987
+  exclusion.robots: 0.115
+  exclusion.robots.policy: 0.107
+  RedisCDXSource: 0.634
+  esindex: 0.009
+  LoadShardBlock: 3523.758 (3)
+  PetaboxLoader3.resolve: 128.837 (2)
+  PetaboxLoader3.datanode: 3690.93 (4)
+  CDXLines.iter: 19.385 (3)
+  load_resource: 306.832
+*/
